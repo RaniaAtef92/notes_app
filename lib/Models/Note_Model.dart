c6d83b167f47a6f455ab478e21165068f74ed9d1
RaniaAtef92 <ronaroro55555@gmail.com>
2023-02-26 20:21:10 +0200

    add hive

---
+++ lib/Models/Note_Model.dart
@@ -0,0 +1,9 @@
+class NoteModel {
+  final String tital;
+  final String subtital;
+  final String date;
+  final int color;
+
+  NoteModel({required this.tital,required this.subtital,required this.date,required this.color});
+  
+}
